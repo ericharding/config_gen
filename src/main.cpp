470e60828210d10b967e34b1354583d00ee43844
Eric Harding <eric@digitalsorcery.net>
2024-10-11 13:41:57 -0400

    Test that we actually parsed stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include "example-config.h"
 
 #include <iostream>
+#include <fstream>
 
 int main(int, char**) {
-    std::cout << "Hello world" << std::endl;
-    return 0;
+  const char* configFilePath = "/home/eric/dev/be/config_gen/config/talon_localhost.json";
+  std::ifstream stream(configFilePath, std::ios::in);
+  if (stream.bad()) {
+    std::cerr << "Config file '" << configFilePath << "' not found.\n";
+    throw std::runtime_error("Failed to open config");
+  }
+  auto doc = nlohmann::json::parse(stream);
+  auto value = parse_config(doc);
+
+  std::cout << "success" << std::endl;
+  std::cout << value.gateway.ip << ":" << value.gateway.port << std::endl;
+  std::cout << "username: " << value.gateway.username << std::endl;
+  return 0;
 }
